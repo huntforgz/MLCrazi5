9c8b2ec07fc65717faff8a25982084faa80bfbe5
mahaoxiang <wpimhx@gmail.com>
2018-11-14 01:17:23 -0500

    modify the folder of the image

--- reshapeimage.py
+++ reshapeimage.py
@@ -15,15 +15,19 @@ def getimage(file_dir):
 if __name__ == '__main__':
     n = -1
     aa = os.getcwd()
-    dirpath = os.path.join(aa, 'cvimage')
+    dirpath = os.path.join(aa, 'cvimage')    #cvimage should be replaced by the folder that contains origin image
     imagedic = getimage(dirpath)
-
+    print(aa)
+    print(type(aa))
     try:
         for key, value in imagedic.items():
             img = cv2.imread(value)
             img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             img2 = cv2.resize(img1, (w, h))
-            cv2.imwrite('pos' + str(n + 1).rjust(3, '0') + '.jpg', img2)
+            #cv2.imwrite('pos' + str(n + 1).rjust(3, '0') + '.jpg', img2)
+            imgname = aa + '\grayimage\pos' + str(n+1) +'.jpg'       #grayimage is the folder that you wish to save the image
+            #print(imgname)
+            cv2.imwrite(imgname, img2)
             n += 1
     except KeyboardInterrupt:
-        print('暂停一下')
+        print('wait')
